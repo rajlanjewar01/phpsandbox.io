f6d49ca3126f6835d1504d0b6eaa857a16bea0c4
raj lanjewar <rajl.perficient@gmail.com>
2022-06-24 12:12:43 +0000

    Initial commit from PHPSandbox

---
+++ index.php
@@ -0,0 +1,17 @@
+<?=
+if(isset($_POST['sbt-btn'])){
+    $value = $_POST['name'];
+
+    $regx = "/^[a-z]$/"
+}
+
+?>
+
+<html>
+<body>
+    <form action="" method="post">
+        <input type="text" name="name">
+        <button type="submit" name="sbt-btn">Submit</button>
+    </form>
+</body>
+</html>
